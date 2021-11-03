174b30d9f4e1d4fe83b5d1dc2c7be49a7d0b9a61
Timothy <timsheppard97@yahoo.com>
2021-11-03 16:11:46 -0400

    pokeapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [poke, setPoke] = useState([])
+  const [showPoke, setShowPoke] = useState(false)
+
+  useEffect(()=> {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1200")
+    .then(res=>{
+      res.json().then(res=> {
+        setPoke(res.results)
+      }).catch(err=>{
+        console.log('err', err)
+      })
+      })
+    .catch(err=>{console.log(err)})
+  },[])
+
+  const display = (e) => {
+    e.preventDefault();
+    setShowPoke(true)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PokeAPI</h1>
       </header>
+        <button onClick={display}>Show Pokemon</button>
+      <ul>
+        {showPoke&&poke.map((pokemon)=>{
+          return <li>{pokemon.name}</li>
+        })}
+      </ul>
     </div>
   );
 }
